1e2828b2f2f4d671721902d9a08d5af356e82da5
PoundforPound <a.zenit2014@yandex.ru>
2024-07-21 12:30:23 +0300

    gallery

---
+++ gallery.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Галерея</title>
+    <style>
+        .gallery {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            grid-gap: 10px;
+        }
+
+        .gallery img {
+            width: 100%;
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="gallery">
+        <?php
+     $user= 'root';
+		 $password= 'root';
+		 $dsn = "mysql:host=localhost;dbname=lesson5";
+		 try{
+			 $pdo= new PDO($dsn,$user,$password);
+			 if($pdo){
+				 $sql = "SELECT * FROM images";
+					$statment = $pdo->query($sql);
+				 $result= $statment->fetchAll(PDO::FETCH_ASSOC);
+					var_dump($result);
+			 }
+		 } catch(PDOException $e){
+			 echo $e->getMessage();
+		 }
+        if ($result->num_rows > 0) {
+            while($row = $result) {
+                echo '<img src="["filename"]' . $row["image_name"] . '" alt="' . $row["image_alt"] . '">';
+            }
+        } else {
+            echo "0 результатов";
+        }
+
+        $conn->close();
+        ?>
+    </div>
+</body>
+</html
--- home5.php
+++ home5.php
@@ -1,10 +1,48 @@
 <?php
+//Get data
+$allowedTypes = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
+$detectedType = exif_imagetype($_FILES['img']['tmp_name']);
+if(in_array($detectedType,$allowedTypes)){
+	move_uploaded_file($_FILES['img']['tmp_name'], 'uploads/' . $_FILES['img']['name']);
+} else{
+	echo 'You chose incorrect file ';
+}
+$userName= $_POST['userName'];
+$description = $_POST['description'];
+$filesName = $_FILES['img']['name'];
+// var_dump($filesName);
+$user= 'root';
+$password= 'root';
+$dsn = "mysql:host=localhost;dbname=lesson5";
 
-$img = $_FILES['img'];
-$imgInfo = exif_imagetype($img);
-// $userName= $_POST['userName'];
-// $description = $_POST['description'];
-
-
-// var_dump($imgInfo);
+//Work with sql
+try{
+	$pdo= new PDO($dsn,$user,$password);
+	if($pdo){
+		// echo "connected to the lesson5 successfully";
+		$sql = "INSERT INTO images (filename, username, description) VALUES (:filesName,:username,:description)";
+		$statment = $pdo->prepare($sql);
+		$statment->execute([
+			":filesName"=> $filesName,
+			":username"=>$userName,
+			":description"=>$description,
+		]);
+ $result = $pdo->lastInsertId();
+//  var_dump($result);
+	}
+} catch(PDOException $e){
+	echo $e->getMessage();
+}
+// get data from sql
+try{
+	$pdo= new PDO($dsn,$user,$password);
+	if($pdo){
+		$sql = "SELECT * FROM images";
+ 		$statment = $pdo->query($sql);
+		$result= $statment->fetchAll(PDO::FETCH_ASSOC);
+ 		var_dump($result);
+	}
+} catch(PDOException $e){
+	echo $e->getMessage();
+}
 
